d1cd97dfc86a629fa7498b053afc87f024f226aa
vidyasagar gaikwad <vidyasagargaikwad7128@gmail.com>
2021-09-02 17:25:49 +0530

    Added : Leap Year

--- Day5_Mix/CustomPrograms.cs
+++ Day5_Mix/CustomPrograms.cs
@@ -6,39 +6,19 @@ namespace Day5_Mix
 {
     class CustomPrograms
     {
-        int count = 1;
-        int sum = 0;
-        int headCount = 0;
-        int tailCount = 0;
-        public void randomNumber()
+        public void leapYear()
         {
-            Console.WriteLine("How many times flip coint..");
+            Console.WriteLine("Enter Year in Four Digits..");
             String number = Console.ReadLine();
-            int n = int.Parse(number);
-
-            while (this.count <= n)
+            int year = int.Parse(number);
+            if (year % 400 == 0 && year % 100 != 0 || year % 4 == 0)
             {
-                int rand = new Random().Next(0, 2);
-                if (rand == 0)
-                {
-                    Console.WriteLine("HEAD");
-                    this.headCount = this.headCount + 1;
-                }
-                else
-                {
-                    Console.WriteLine("TAIL");
-                    this.tailCount = this.tailCount + 1;
-                }
-                count++;
+                Console.WriteLine($"{year} : is Leap Year..!");
             }
-            Console.WriteLine($"Head : {this.headCount}");
-            Console.WriteLine($"Tail : {this.tailCount}");
-            this.sum = this.headCount + this.tailCount;
-            double avg = ((double)this.headCount / this.sum * 100);
-            double otherAvg = 100 - avg;
-            Console.WriteLine($"HEAD Percentage % ::::: {avg}");
-            Console.WriteLine($"TAIL Percentage % ::::: {otherAvg}");
-            Console.WriteLine($"SUM ::::: {this.sum}");
+            else
+            {
+                Console.WriteLine($"{year} : is Not Leap Year..!");
+            }
         }
     }
 }
