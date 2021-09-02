a77cdaed7a25808693dd9ee6cee49070e0fa9ccc
vidyasagar gaikwad <vidyasagargaikwad7128@gmail.com>
2021-09-02 16:56:43 +0530

    Added : Flip coin,Percentage of Head and Tail

---
+++ Day5_Mix/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Day5_Mix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Day Five Mix Problems..!");
+        }
+    }
+}
